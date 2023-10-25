4116d08d98efc05b63146ec03fc1aa9f791525bf
k4r0lina <104777013+k4r0lina@users.noreply.github.com>
2023-10-25 18:02:39 -0300

    dddd

--- main.js
+++ main.js
@@ -9,3 +9,15 @@ function tocaSomClap() {
 }
 
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+function tocaSomTim() {
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+function tocaSomPuff() {
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla_puff').onclick = tocaSomPuff;
