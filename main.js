ecc43db906b3986452e553d69bff4733a838661e
k4r0lina <104777013+k4r0lina@users.noreply.github.com>
2023-10-25 17:39:39 -0300

    koooo

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 document.querySelector('.tecla_pom');
-function tocaSomPom(){
-document.querySelector('#som_tecla_pom').play();
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
 }
