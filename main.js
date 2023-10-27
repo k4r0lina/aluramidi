7ec08a63bd08e37df9d4dd5700dc8e227c7acc32
k4r0lina <104777013+k4r0lina@users.noreply.github.com>
2023-10-27 16:03:04 -0300

    dddd

--- main.js
+++ main.js
@@ -2,30 +2,30 @@ function TocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_pom').onclick = TocaSomPom;
 
 function TocaSomClap() {
     document.querySelector('#som_tecla_clap').play();
 }
 
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
+document.querySelector('.tecla_clap').onclick = TocaSomClap;
 
 function TocaSomTim() {
     document.querySelector('#som_tecla_tim').play();
 }
 
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
+document.querySelector('.tecla_tim').onclick = TocaSomTim;
 
 function TocaSomPuff() {
     document.querySelector('#som_tecla_puff').play();
 }
 
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+document.querySelector('.tecla_puff').onclick = TocaSomPuff;
 
 function TocaSomSplash() {
     document.querySelector('#som_tecla_splash').play();
 }
 
-document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+document.querySelector('.tecla_splash').onclick = TocaSomSplash;
 
 
