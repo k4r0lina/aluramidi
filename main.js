d06abecd09886a1ec446cb6cc58f384b0dacc039
k4r0lina <104777013+k4r0lina@users.noreply.github.com>
2023-10-27 16:02:36 -0300

    cccc

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-function tocaSomPom() {
+function TocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
-function tocaSomClap() {
+function TocaSomClap() {
     document.querySelector('#som_tecla_clap').play();
 }
 
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
-function tocaSomTim() {
+function TocaSomTim() {
     document.querySelector('#som_tecla_tim').play();
 }
 
 document.querySelector('.tecla_tim').onclick = tocaSomTim;
 
-function tocaSomPuff() {
+function TocaSomPuff() {
     document.querySelector('#som_tecla_puff').play();
 }
 
 document.querySelector('.tecla_puff').onclick = tocaSomPuff;
+
+function TocaSomSplash() {
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+
